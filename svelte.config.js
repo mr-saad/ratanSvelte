56bb70e94af203391762156a0b45f11a8da2d251
saadkhatri1 <khatrisaad233@gmail.com>
2024-12-03 00:12:13 +0530

    Update svelte.config.js 2

--- svelte.config.js
+++ svelte.config.js
@@ -11,14 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			images: [
-				{
-					domains: ["cdn.sanity.io"],
-					formats: ["image/avif", "image/webp"]
-				}
-			]
-		})
+		adapter: adapter({})
 	}
 }
 
