7e7e3a959f6acf23bae85a3f1576156d014cc863
saadkhatri1 <khatrisaad233@gmail.com>
2024-12-03 00:16:18 +0530

    update store.js

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,11 +1,12 @@
 import { writable } from "svelte/store"
 import type { Auth } from "../types"
-import { env } from "$env/dynamic/public"
+// import { env } from "$env/dynamic/public"
+const VERCEL_URL = "https://ratan-svelte.vercel.app"
 
 export const auth = writable<Auth>({ email: "", status: false, userId: "", username: "", cart: [] })
 
 async function get() {
-	const res = await (await fetch(env.PUBLIC_VERCEL_URL + "/api/getAuth")).json()
+	const res = await (await fetch(VERCEL_URL + "/api/getAuth")).json()
 	auth.set(res)
 }
 get()
