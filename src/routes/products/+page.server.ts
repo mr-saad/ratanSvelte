ad6ae2f4377449a8fcf77bc9e469410c1f625ac7
saadkhatri1 <khatrisaad233@gmail.com>
2024-12-03 12:42:11 +0530

    native fetch in products/+page.server.ts

--- src/routes/products/+page.server.ts
+++ src/routes/products/+page.server.ts
@@ -2,9 +2,9 @@ import { env } from "$env/dynamic/private"
 import type { Prod } from "../../types"
 import type { PageServerLoad } from "./$types"
 
-export const load: PageServerLoad = async (req) => {
+export const load: PageServerLoad = async () => {
 	const q = `*[_type=="product"]{_id,"slug":slug.current,title,price,"image":images[0].asset->{url}}`
 	const prodsStream = async (): Promise<Prod[]> =>
-		(await (await req.fetch(env.queryUrl + `?query=${encodeURIComponent(q)}`)).json()).result
+		(await (await fetch(env.queryUrl + `?query=${encodeURIComponent(q)}`)).json()).result
 	return { prodsStream: prodsStream() }
 }
